4ca4fa0cabe0125ae41a28393b9e7fcf48e61dcc
Dima <briksindima@gmail.com>
2021-03-09 20:19:19 +0100

    Logging. log4j.xml

--- src/main/java/ru/home/HibernateRunner.java
+++ src/main/java/ru/home/HibernateRunner.java
@@ -1,11 +1,16 @@
 package ru.home;
 
+
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import ru.home.entity.User;
 import ru.home.util.HibernateUtil;
 
 public class HibernateRunner {
+
+    private static final Logger log = LoggerFactory.getLogger(HibernateRunner.class);
 
     public static void main(String[] args) {
         User usr = User.builder()
@@ -13,11 +18,16 @@ public class HibernateRunner {
                 .lastname("Ivanov")
                 .firstname("Ivan")
                 .build();
+        log.info("Uses entity is in transient state, object: {}", usr.getUsername());
 
         try (SessionFactory sessionFactory = HibernateUtil.buildSessionFactory()) {
             try (Session session1 = sessionFactory.openSession()) {
                 session1.beginTransaction();
+                log.trace("Transaction is created, {}", usr);
+
                 session1.saveOrUpdate(usr);
+                log.trace("Uses is in persistent state: {}, session {}", usr, session1);
+
                 session1.getTransaction().commit();
             }
 
